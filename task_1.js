21527cc2a8c105db011d24c89227e2fdac57eb99
Sashok9203 <kovalchuk.olex@gmail.com>
2023-12-05 14:20:11 +0200

    Update

---
+++ task_1.js
@@ -0,0 +1,6 @@
+
+const current_year = 2023;
+let user_year = prompt("Enter your year of birth:");
+alert(`Your age is ${current_year -  user_year} years`);
+let user_spase = prompt("Enter the volume of the flash drive(Gb):");
+alert(`${parseInt((user_spase*1024)/820) } files with a size of 820MBb can fit on a flash drive`);
